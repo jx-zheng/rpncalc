ef0ac70c956cea953b901d3f20728e4cf9f2c288
Kevin Zheng <jxzheng@uwaterloo.ca>
2021-06-02 18:11:24 -0400

    pylint linting

---
+++ rpn_cli_driver.py
@@ -0,0 +1,75 @@
+# RPNCalculator.py
+# Written by Kevin Zheng
+# September 2018
+# Description: CLI driver for rpncalc
+
+import time
+import rpn_functions
+
+user_input = "" # Initialize the user input string
+
+print("Welcome to RPNCalculator.".center(80, " ")) # Centers the title with whitespace
+
+time.sleep(1.5) # Pause for dramatic effect
+
+print("For help, please enter 'help' at any time.".center(80, " "))
+print("Default stack size is 4. To change the size of the stack, enter 'cs'.".center(80, " "))
+
+while user_input.upper() != "QUIT":
+    user_input = input() # Accept user input from the console
+    if user_input == "help":
+        rpn_functions.show_help()
+    elif user_input.upper() == "QUIT": # Causes the exit message to be printed
+        pass
+    elif user_input == "": # Pushing 0 to stack (blank or 0 input)
+        rpn_functions.push(0)
+    elif user_input == "ss": # Print the value of all registers in the stack
+        rpn_functions.show_stack()
+    elif user_input == "cs": # Change the number of usable registers in the stack
+        rpn_functions.change_stack_size()
+    elif user_input == "sws": # Swap the top two values in the stack
+        rpn_functions.swap()
+        rpn_functions.print_top()
+    elif user_input == "cls": # Clear the stack by setting every register to 0
+        rpn_functions.clear_stack()
+    elif user_input == "pop": # Pop the last value from the top of the stack
+        rpn_functions.pop()
+        rpn_functions.print_top()
+    elif user_input == "sq": # Square the last value in the stack
+        rpn_functions.square()
+        rpn_functions.print_top()
+    elif user_input == "^": # Compute a power using the last two values in the stack
+        rpn_functions.power()
+        rpn_functions.print_top()
+    elif user_input == "sqrt": # Square root the last value in the stack
+        rpn_functions.square_root()
+        rpn_functions.print_top()
+    elif user_input == "nrt": # Compute the nth root using the last two values in the stack
+        rpn_functions.nth_root()
+        rpn_functions.print_top()
+    elif user_input == "info": # Prints information about RPN Calculators to the console
+        rpn_functions.show_info()
+    elif user_input == "+": # Sum of last two numbers in the stack
+        rpn_functions.add()
+        rpn_functions.print_top()
+    elif user_input == "-": # Difference of last two numbers in the stack
+        rpn_functions.subtract()
+        rpn_functions.printTop()
+    elif user_input == "*": # Product of last two numbers in the stack
+        rpn_functions.multiply()
+        rpn_functions.print_top()
+    elif user_input == "/": # Divide last two numbers in the stack
+        rpn_functions.divide()
+        rpn_functions.print_top()
+    elif user_input == "rec": # Compute the reciprocal of the last value in the stack
+        rpn_functions.reciprocal()
+        rpn_functions.print_top()
+    elif user_input == "clear":
+        rpn_functions.clear() # Clear the console screen
+    else:
+        try:
+            rpn_functions.push(float(user_input)) # Try to evaluate it as push
+        except (TypeError, ValueError):
+            print("Invalid command or value: enter 'help' for command help.")
+
+print("Thank you for using RPNCalculator.")
---
+++ rpn_functions.py
@@ -0,0 +1,156 @@
+# RPNFunctions.py
+# Written by Kevin Zheng
+# September 2018
+# Description: Contains the essential functions for RPNCalculator
+
+import math
+from os import system, name
+
+stack = [0, 0, 0, 0]
+max_size = 4 # Define the maximum size of the default stack
+
+def show_help():
+    print("")
+    print("- Functions of RPNCalculator - ".center(80, " ")) # Centers the title using whitespace
+    print("\n'help': Explains all functions and their associated commands",
+          "'quit': Exit RPNCalculator",
+          "'clear': Clear the screen",
+          "'+', '-', '*', '/': Add, subtract, multiply, and divide; respectively",
+          "'ss': Shows the stack at the time of the command execution",
+          "'cs': Change the size of the stack. This will also clear the stack",
+          "'sws': Swap the positions of the topmost 2 values in the stack",
+          "'cls': Clear the stack",
+          "'pop': Pop the topmost value from the stack",
+          "'sq': Square the topmost value in the stack",
+          "'^': Exponent operation with topmost 2 values in the stack",
+          "'sqrt': Square root operation with the topmost value in the stack",
+          "'nrt': nth root operation with the topmost two values in the stack",
+          "'rec': Reciprocal operation with the topmost value in the stack",
+          "'info': Show information about RPNCalculator", sep="\n")
+
+def push(x):
+    shift_up()
+    stack[len(stack) - 1] = x # Set the last value in the stack to the new value input by the user
+
+def pop():
+    popholder = len(stack) - 1 # Temporarily hold the last value in the stack
+    for popvalue in reversed(stack): # Iterate through the stack backwards
+        stack[popholder] = stack[popholder - 1]
+        popholder -= 1
+        if popholder == 0: # This prevents trying to read the -1th index of the stack
+            stack[0] = 0
+            break
+
+def show_stack():
+    print("=====")
+    print(*stack, sep="\n") # Prints each value of the stack on a new line
+    print("=====")
+
+def change_stack_size():
+    global max_size
+    accept_input = 0
+    while accept_input == 0:
+        max_size = input("Please enter a value for the stack size. ")
+        try:
+            max_size = int(max_size)
+        except:
+            print("Invalid stack size: please enter a positive integer greater than 1.")
+            max_size = 4
+        else: # If no exception was thrown when casting to integer, this executes
+            if max_size < 2:
+                print("Invalid stack size: please enter a positive integer greater than 1.")
+            else:
+                accept_input = 1
+                stack.clear() # Reset all stack values to 0
+                for _ in range(0, max_size):
+                    stack.append(0) # Append new values to the list to change the size of the stack
+                    if len(stack) == max_size:
+                        print("Stack size successfully changed to {0} registers.".format(max_size))
+                        break
+
+def swap(): # Swap the values of the top two registers in the stack
+    swapholder = stack[(len(stack)-2)]
+    stack[(len(stack)-2)] = stack[(len(stack)-1)]
+    stack[(len(stack)-1)] = swapholder
+
+def clear_stack(): # Set all registers in the stack equal to zero
+    for cleanup in range(0, max_size):
+        stack[cleanup] = 0
+    print("Stack cleared.")
+
+def square():
+    stack[(len(stack) - 1)] = math.pow(stack[(len(stack) - 1)], 2)
+
+def power():
+    exponent = stack[(len(stack) - 1)]
+    base = stack[(len(stack) - 2)]
+    pop() # Shift everything down so that we can combine the last two registers into one register
+    stack[(len(stack) - 1)] = math.pow(base, exponent)
+
+def square_root():
+    try:
+        stack[(len(stack) - 1)] = math.sqrt(stack[(len(stack) - 1)])
+    except ValueError:
+        print("Domain error (square root of negative number)")
+
+def nth_root():
+    index = stack[(len(stack) - 1)]
+    radicand = stack[(len(stack) - 2)]
+    pop()
+    stack[(len(stack) - 1)] = math.pow(radicand, (1/index))
+
+def show_info():
+    print("Want to learn to use RPN (Reverse Polish Notation) calculators?",
+          "Visit https://www.lehigh.edu/~sgb2/rpnTutor.html (teaches you the HP-12C calculator).",
+          "This program was written by Kevin Zheng in 2018.", sep="\n")
+
+def add():
+    value1 = stack[(len(stack) - 1)]
+    value2 = stack[(len(stack) - 2)]
+    pop()
+    stack[(len(stack) - 1)] = value1 + value2
+
+def subtract():
+    value1 = stack[(len(stack) - 1)]
+    value2 = stack[(len(stack) - 2)]
+    pop()
+    stack[(len(stack) - 1)] = value2 - value1
+
+def multiply():
+    value1 = stack[(len(stack) - 1)]
+    value2 = stack[(len(stack) - 2)]
+    pop()
+    stack[(len(stack) - 1)] = value1 * value2
+
+def divide():
+    try:
+        value1 = stack[(len(stack) - 1)]
+        value2 = stack[(len(stack) - 2)]
+        pop()
+        stack[(len(stack) - 1)] = value2 / value1
+    except ZeroDivisionError:
+        print("Error (Dividing by zero)")
+
+def reciprocal():
+    try:
+        stack[(len(stack) - 1)] = 1 / stack[(len(stack) - 1)]
+    except ZeroDivisionError:
+        print("Error (Dividing by zero)")
+
+def shift_up(): # Pops from the bottom of the stack
+    stackcounter = 0
+    for _ in stack:
+        stack[stackcounter] = stack[stackcounter + 1]
+        stackcounter += 1
+        if stackcounter == (len(stack) - 1):
+            stack[len(stack) - 1] = 0
+            break
+
+def print_top(): # Prints the top/last value in the stack
+    print(stack[len(stack) - 1])
+
+def clear(): # Clears console screen
+    if name == 'nt': # Windows
+        _ = system('cls')
+    else: # Other operating sysems
+        _ = system('clear')
